2c240094f870cc25c2ad22e48b6ae6c66fc0a469
Masud Rana <masudranaonline75@gmail.com>
2023-04-03 15:24:55 +0600

    php output

---
+++ output/echo_print.php
@@ -0,0 +1,9 @@
+<?php
+
+	$text = "Bangladesh is a beautifull country";
+	$text2 = "Bangladesh";
+
+	echo "<h1> Hello Bangladesh </h1>";
+	echo $text. "<br>";
+	print $text2. " is a beautifull country";
+
