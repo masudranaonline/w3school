b79d0764172381c4dbabe22c362d71c02104e8f1
Masud Rana <masudranaonline75@gmail.com>
2023-04-08 17:08:55 +0600

    string done

--- php string/string.php
+++ php string/string.php
@@ -8,3 +8,16 @@
 
 	echo "Count Words in a String: 'Masud Rana' <br>";
 	echo str_word_count("Masud Rana");
+	echo "<br>";
+
+	echo "Reverse a String: 'Masud' <br>";
+	echo strrev("Masud");
+	echo "<br>";
+
+	echo "Search For a Text Within a String: 'Bangladesh is a beautifull country' <br> ";
+	echo strpos("Bangladesh is a beautifull country", "beautifull");
+	echo "<br>";
+
+	echo "Replace the text: Hello Masud! Hello Words <br>";
+
+	echo str_replace("Words", "Masud", "Hello Words");
