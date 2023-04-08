e049949cec65e3e245bbbafabe9b20f1786ac049
Masud Rana <masudranaonline75@gmail.com>
2023-04-08 16:57:08 +0600

    php

---
+++ php string/string.php
@@ -0,0 +1,10 @@
+<?php
+
+
+	echo "Return the length of the string: 'Masud Rana' <br>";
+	
+	echo strlen("Masud Rana");
+	echo "<br>";
+
+	echo "Count Words in a String: 'Masud Rana' <br>";
+	echo str_word_count("Masud Rana");
