36800ece78d10b3364936c788c42c318306328cd
Masud Rana <masudranaonline75@gmail.com>
2023-04-08 16:28:38 +0600

    problem solved

--- data type/data_type.php
+++ data type/data_type.php
@@ -6,3 +6,18 @@
 
 	echo "<br>";
 	echo "this is a flot data type: " ,4.258;
+	echo "<br>";
+	echo "<br>";
+
+	$text = "Masud Rana";
+	$num = 54654;
+	$num1 = 52.3645;
+	$cars = array('volvo','bmw','toyota');
+
+	var_dump($text);
+	echo "<br>";
+	var_dump($num);
+	echo "<br>";
+	var_dump($num1);
+	echo "<br>";
+	var_dump($cars);
