6b345b05f76cbcd451604363a9b6a09db26f106b
Masud Rana <masudranaonline75@gmail.com>
2023-04-29 23:51:26 +0600

    practies done

---
+++ number/number.php
@@ -0,0 +1,13 @@
+<?php
+
+	$x = 4556465;
+	 var_dump(is_numeric($x));
+	 echo "<br>";
+
+	 $y = 356.1654;
+	 var_dump(is_numeric($y));
+	 echo "<br>";
+
+
+	 $z = "Hello";
+	 var_dump(is_numeric($z));
