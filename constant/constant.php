8afe8c91f7fd1afad010debbd96b9b0376e73919
Masud Rana <masudranaonline75@gmail.com>
2023-04-30 00:17:49 +0600

    w3School constant

---
+++ constant/constant.php
@@ -0,0 +1,21 @@
+<?php
+
+	echo "PHP Constants <br>";
+
+    // define("rana", "Welcome to City Polytechnic Instute");
+
+    // echo rana;
+
+    // define("cars", [
+    // 	"Volvo",
+    // 	"BMw",
+    // 	"Toyota"
+    // ]);
+    // echo cars[1];
+
+    define("rana", "I loved Bangladesh");
+
+    function myMasud(){
+    	echo rana;
+    }
+    myMasud();
