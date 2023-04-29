e4bdd8d832679383403c7ba8b0b48dd0d7e9082c
Masud Rana <masudranaonline75@gmail.com>
2023-04-30 00:05:51 +0600

    w3school

---
+++ php math/math.php
@@ -0,0 +1,31 @@
+<?php
+
+echo " PHP pi() Function <br>";
+echo pi();
+echo "<br>";
+echo "<br>";
+
+
+echo "PHP min() and max() Functions <br>";
+echo min(200,180,300,15,-458);
+echo "<br>";
+echo max(-584,15,38,200,280);
+echo "<br>";
+echo "<br>";
+
+
+echo "PHP abs() Function <br>";
+echo abs(-6.7);
+echo "<br>";
+echo "<br>";
+echo "<br>";
+
+echo "PHP sqrt() Function <br>";
+echo sqrt(49);
+echo "<br>";
+echo "<br>";
+
+echo rand();
+
+
+
