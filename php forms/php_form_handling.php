78da85a7abdd79cdea778267899a1f37865d905d
Masud Rana <masudranaonline75@gmail.com>
2023-05-07 00:04:04 +0600

    $_get

--- php forms/php_form_handling.php
+++ php forms/php_form_handling.php
@@ -4,19 +4,29 @@
 
 
 
-	<form action="php_form_handling.php" method="post">
+<!-- 	<form action="php_form_handling.php" method="post">
 		Name: <input type="text" name="name"><br><br>
 		Name: <input type="text" name="email"><br>
 		<input type="submit">
-	</form>
+	</form> -->
 
 	<!-- <?php echo $_POST["name"]; ?><br>
 	<?php echo $_POST["email"]; ?> -->
 
 
 
-	Welcome <?php echo $_POST["name"]; ?><br>
-	Your email address is: <?php echo $_POST["email"]; ?>
+<!-- 	Welcome <?php echo $_POST["name"]; ?><br>
+	Your email address is: <?php echo $_POST["email"]; ?> -->
+
+
+	<form action="php_form_handling.php" method="get">
+		Name : <input type="text" name="name"><br><br>
+		E-mail : <input type="text" name="email"><br><br>
+		<input type="submit">
+		<h1>Welcome : <?php echo $_GET['name']; ?></h1>
+	<h1>Your mail address id : <?php echo $_GET['email']; ?></h1>
+	</form>
+
 
 
 </body>
