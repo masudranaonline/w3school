cda3cdd1b7bfbcf560e93e863e2d4077407a3de5
Masud Rana <masudranaonline75@gmail.com>
2023-05-12 18:15:10 +0600

    date & time

--- php Advance/Date &Time/date.php
+++ php Advance/Date &Time/date.php
@@ -17,5 +17,46 @@
     echo "<br>";
     echo "<br>";
     
-
+    date_default_timezone_set("Asia/Dhaka");
     echo "The time is : ". date("h:i:sa");
+    echo "<br>";
+    echo "<br>";
+    echo "<br>";
+
+    echo "Create a Date With mktime() <br>";
+    $d = mktime(5, 58, 15, 5,12, 2023);
+    echo "Created date is ". date("Y-m-d; h:i:sa",$d);
+
+
+    echo "<br>";
+    echo "<br>";
+    echo "<br>";
+
+    echo "Create a Date From a String With strtotime()";
+    $a = strtotime("10:30pm April 15 2014");
+    echo "Create date is ". date("Y-m-d h:i:sa",$a);
+
+    echo "<br>";
+    echo "<br>";
+    echo "<br>";
+
+    $d = strtotime("tomorrow");
+    echo date("Y-m-d h:i:sa", $d);
+    echo "<br>";
+    $d = strtotime("next Sunday");
+    echo date("Y-m-d h:i:sa",$d);
+    echo "<br>";
+    $d = strtotime("+3Months");
+    echo date("Y-m-d h:i:sa", $d);
+    echo "<br>";
+
+
+
+
+
+
+
+
+
+
+
