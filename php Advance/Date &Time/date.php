2c608fae39ce8da49e0cfb8d7f8a34bac7965387
Masud Rana <masudranaonline75@gmail.com>
2023-05-12 17:08:26 +0600

    date & time

---
+++ php Advance/Date &Time/date.php
@@ -0,0 +1,21 @@
+<?php
+    echo "Tooday is " . date("Y/m/d");
+    echo "<br>";
+    echo "Tooday is " . date("Y,m,d");
+    echo "<br>";
+    echo "Tooday is " . date("Y-m-d");
+    echo "<br>";
+    echo "Tooday is " . date("Y.m.d");
+    echo "<br>";
+    echo date("l");
+    echo "<br>";
+    echo "<br>";
+    echo "<br>";
+
+    echo "©2010-". date("Y");
+    echo "<br>";
+    echo "<br>";
+    echo "<br>";
+    
+
+    echo "The time is : ". date("h:i:sa");
